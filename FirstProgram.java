0ab423cd4265061757025b991e97c8c7c61e5fa5
Prateek-Gokhale <prateekgokhale35@gmail.com>
2026-02-12 19:48:11 +0530

    Initial commit - Added Java Basics Hello World programs

---
+++ FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+  public static void main(String[] args) {
+    System.out.println("Welcome");
+    System.out.println("back to Java programming");
+    System.out.println("Practice makes perfect");
+  }
+}
