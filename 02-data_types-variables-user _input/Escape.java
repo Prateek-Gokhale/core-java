961057e7984ab0b0e2a75855718f3832c38c20e7
Prateek-Gokhale <prateekgokhale35@gmail.com>
2026-02-13 19:22:09 +0530

    Added three basic programs

---
+++ 02-data_types-variables-user _input/Escape.java
@@ -0,0 +1,11 @@
+public class Escape {
+
+    public static void main(String[] args) {
+        System.out.println("Hello \"World\"!");
+        System.out.println("This is a new line.\nAnd this is another line.");
+        System.out.println("This is a tab:\tTabbed text.");
+        System.out.println("This is a backslash: \\");
+        System.out.println("Hello\bJavaCoding...");
+    }
+}
+
